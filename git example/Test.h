c56bcf2992eca2be3cfdcb8a379927b7649de484
coolzat <coolzat@coolzats-MacBook-Air-2.local>
2014-12-12 15:19:24 +0000

    added a test class

---
+++ git example/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  git example
+//
+//  Created by coolzat on 12/12/14.
+//  Copyright (c) 2014 iCZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ git example/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  git example
+//
+//  Created by coolzat on 12/12/14.
+//  Copyright (c) 2014 iCZ. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
