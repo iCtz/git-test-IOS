6f8f3e7d5829a96625b3b8c378755d8dc3a4023b
coolzat <coolzat@coolzats-MacBook-Air-2.local>
2014-12-12 14:20:10 +0000

    Initial Commit

---
+++ git example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git example
+//
+//  Created by coolzat on 12/12/14.
+//  Copyright (c) 2014 iCZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git example
+//
+//  Created by coolzat on 12/12/14.
+//  Copyright (c) 2014 iCZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
